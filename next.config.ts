b639f77da8b764305f1fed856681b1950610f9a7
Mohammed Alsir <dreambooy9@gmail.com>
2025-09-11 09:44:29 +0000

    Try fixing this error: `Runtime Error: Error: window is not defined. Err

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  transpilePackages: ['react-leaflet'],
+  transpilePackages: ['react-leaflet', 'leaflet-defaulticon-compatibility'],
   typescript: {
     ignoreBuildErrors: true,
   },
