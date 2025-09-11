60fee3440c52d81fcbc7dea59e4e4680d6c6455b
Mohammed Alsir <dreambooy9@gmail.com>
2025-09-11 09:48:21 +0000

    Try fixing this error: `Runtime Error: Error: Map container is already i

--- src/components/map-modal.tsx
+++ src/components/map-modal.tsx
@@ -14,6 +14,7 @@ import type { LatLngBoundsExpression } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
 import "leaflet-defaulticon-compatibility";
+import { useEffect, useState } from 'react';
 
 
 interface MapModalProps {
@@ -31,7 +32,13 @@ const MapBounds = ({ bounds }: { bounds: LatLngBoundsExpression }) => {
 };
 
 const MapModal = ({ isOpen, onClose, weatherDataList, tempUnit }: MapModalProps) => {
-  if (!isOpen || weatherDataList.length === 0) {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted || !isOpen || weatherDataList.length === 0) {
     return null;
   }
 
